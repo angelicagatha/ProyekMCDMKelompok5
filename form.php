534def52399bdfe86821383e23651abcd65c2899
Angelica Agatha <125953295+angelicagatha@users.noreply.github.com>
2023-06-01 14:14:41 +0700

    form lagi

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
         <link rel="stylesheet" type="text/css" href="styles.css">
     </head>
     <body>
-        <div class="contact-us">
+        <div class="form-input">
             <form>
                 <input placeholder="Name" required="" type="text" /><input name="customerEmail" placeholder="Email" type="email" /><input name="customerPhone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone" type="tel" /><button type="button">SIGN UP</button>
             </form>
